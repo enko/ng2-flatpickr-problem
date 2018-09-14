ecbb18dfdcda28e79d86baa776264da99fddfde4
Tim Schumacher <tim.schumacher@flyacts.com>
2018-09-14 12:26:51 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormBuilder } from '@angular/forms';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flatpickr';
+    title = 'flatpickr';
+
+    name: string;
+    public form: FormGroup;
+    constructor(public formBuilder: FormBuilder) {
+        this.form = this.formBuilder.group({
+            birthday: '',
+        });
+
+        this.form.patchValue({ birthday: new Date() });
+    }
+
+    public submit() {
+        console.log(this.form.value);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Ng2FlatpickrModule } from 'ng2-flatpickr';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    Ng2FlatpickrModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
